cdd2e64f2e823d311b0a591a1ef99efe04e343a4
Kirill Artimenia <kirill.artimenya@mail.ru>
2020-08-29 18:22:38 +0300

    start project

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+// $( document ).ready(function() {
+//   $('.main__slider').slick({
+//     fade: true,
+//     arrows: false,
+//     asNavFor: '.slider__dots',
+//     infinite: true,
+//   })
+//   $('.slider__dots').slick({
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     asNavFor: '.main__slider',
+//     focusOnSelect: true
+//   })
+// });
