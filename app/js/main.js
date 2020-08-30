11677d54432a0526926b0edb8f53e3fbe2511b6c
Kirill Artimenia <kirill.artimenya@mail.ru>
2020-08-30 17:46:48 +0300

    add games section

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,27 @@
-// $( document ).ready(function() {
-//   $('.main__slider').slick({
-//     fade: true,
-//     arrows: false,
-//     asNavFor: '.slider__dots',
-//     infinite: true,
-//   })
-//   $('.slider__dots').slick({
-//     infinite: true,
-//     slidesToShow: 4,
-//     slidesToScroll: 1,
-//     arrows: false,
-//     asNavFor: '.main__slider',
-//     focusOnSelect: true
-//   })
-// });
+$( document ).ready(function() {
+  // $('.main__slider').slick({
+  //   fade: true,
+  //   arrows: false,
+  //   asNavFor: '.slider__dots',
+  //   infinite: true,
+  // })
+  // $('.slider__dots').slick({
+  //   infinite: true,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   asNavFor: '.main__slider',
+  //   focusOnSelect: true
+  // })
+  $('.games-slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    rows: 2,
+    infinite: false,
+    swipe: false,
+    dots: true,
+    prevArrow:'<button type="button" class="slick-prev"><img src="imagines/prev.png" alt=""></button>',
+    nextArrow:'<button type="button" class="slick-next"><img src="imagines/next.png" alt=""></button>',
+    dotsClass: 'games__slider-dots'
+  })
+});
