25a7195109fddb4ba9a99834f0a3babdabfdd2f8
Kirill Artimenia <kirill.artimenya@mail.ru>
2020-09-19 21:54:54 +0300

    add respons

--- app/js/main.js
+++ app/js/main.js
@@ -1,53 +1,11 @@
-$( document ).ready(function() {
-  $('.main__slider').slick({
-    fade: true,
-    arrows: false,
-    asNavFor: '.slider__dots',
-    infinite: true,
-    swipe: false,
-    responsive: [
-      {
-        breakpoint: 630,
-        settings: {
-          swipe: true,
-          fade: false
-        }
-      }
-    ]
-  })
-  $('.slider__dots').slick({
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    arrows: false,
-    asNavFor: '.main__slider',
-    focusOnSelect: true,
-    variableWidth: true,
-    swipe: false
-  })
-  $('.games-slider').slick({
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    rows: 2,
-    infinite: false,
-    swipe: false,
-    dots: true,
-    prevArrow:'<button type="button" class="slick-prev"><img src="imagines/prev.png" alt=""></button>',
-    nextArrow:'<button type="button" class="slick-next"><img src="imagines/next.png" alt=""></button>',
-    dotsClass: 'games__slider-dots dots-style'
-  })
+const menuBtn = document.getElementsByClassName('menu__btn')[0],
+      menuBbody = document.getElementsByClassName('menu__body')[0],
+      btnCloser = document.getElementsByClassName('menu__body-close')[0]
 
-  $('.trailers-slider').slick({
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    rows: 3,
-    infinite: false,
-    swipe: false,
-    dots: true,
-    prevArrow:'<button type="button" class="slick-prev"><img src="imagines/prev.png" alt=""></button>',
-    nextArrow:'<button type="button" class="slick-next"><img src="imagines/next.png" alt=""></button>',
-    dotsClass: 'trailers__slider-dots dots-style',
-    variableWidth:true,
+menuBtn.addEventListener('click', () =>{
+  menuBbody.classList.add("_active");
+});
 
-  })
+btnCloser.addEventListener('click', () =>{
+  menuBbody.classList.remove("_active");
 });
